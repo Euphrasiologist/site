92b1db2456cce14e43fd42a7896a19a4964131f7
Max Brown <max.brown@ed.ac.uk>
2023-09-15 06:30:56 +0100

    Improve logging to file

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,29 @@ use anyhow::Result;
 use axum::{http::StatusCode, routing::get_service, Router};
 use std::net::SocketAddr;
 use tower_http::{services::ServeDir, trace::TraceLayer};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::Level;
+use tracing_appender::rolling;
+use tracing_subscriber::{
+    fmt::writer::MakeWriterExt, layer::SubscriberExt, util::SubscriberInitExt,
+};
 
 use web_server::build_website;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // write to a file daily
+    let info_file = rolling::daily("./logs", "daily.log").with_max_level(tracing::Level::INFO);
+
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
                 .unwrap_or_else(|_| "example_static_file_server=debug,tower_http=debug".into()),
         ))
-        .with(tracing_subscriber::fmt::layer().json())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_writer(info_file),
+        )
         .init();
 
     build_website("content", "public")?;
